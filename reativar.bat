edf95119833659c53c7fc64d8cca9b01ad89015f
Carlos Souza <carlos-souza@PROCERGS.RS.GOV.BR>
2017-05-25 18:11:34 -0300

    Nova alteracoes...

--- reativar.bat
+++ reativar.bat
@@ -13,5 +13,10 @@ echo.
 echo    - Lista suspensa ATUAL:
 git stash list
 echo.
+rem echo    - Buscando e removendo a lista suspensa %1...
+rem git stash pop
+rem echo.
+rem echo    - Cria uma branch a partir do stash
+rem git stash branch {nome}
 echo -----------------------------------------------------------------------
 echo Reativacao feita com sucesso!
