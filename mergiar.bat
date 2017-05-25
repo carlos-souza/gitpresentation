fc25cfa4340b276dd7e3227ba311216a6ee41140
Carlos Souza <carlos-souza@PROCERGS.RS.GOV.BR>
2017-05-25 17:23:28 -0300

    Subindo troca do branch...

---
+++ mergiar.bat
@@ -0,0 +1,23 @@
+@echo off
+cls
+echo.
+echo Trazendo a implementa‡Æo para o MASTER...
+echo -----------------------------------------------------------------------
+echo.
+echo   - Trocando o foco para o MASTER...
+git checkout master
+echo.
+echo   - Verificando a diferen‡a com %1
+git diff %1 master
+
+echo.
+pause
+echo.
+echo   - Fazendo o MERGE...
+git merge %1
+echo.
+echo   - Eliminando o branch (desnecess rio)...
+git branch -d %1
+echo.
+echo -----------------------------------------------------------------------
+echo Pronto!! Tudo em MASTER agora.
